1a18b91a4b38d4af37221ec7b21daf6145d18321
EliseGabilly <elise.gabilly@gmail.com>
2022-02-06 12:03:00 +0100

    Graphics

--- ShatterArenaGame/Assets/_Scripts/Units/Explosion.cs
+++ ShatterArenaGame/Assets/_Scripts/Units/Explosion.cs
@@ -11,7 +11,7 @@ public class Explosion : MonoBehaviour {
     }
 
     private IEnumerator Unactive() {
-        yield return new WaitForSeconds(0.01f);
+        yield return new WaitForSeconds(0.05f);
         gameObject.SetActive(false);
     }
 }
